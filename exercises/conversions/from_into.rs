8e9cfbf5a8a2f75b3a4dc14bd7b32cb4588c5f8e
yuguohua <ghy_hust@qq.com>
2023-10-20 10:17:23 +0800

    96%

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        } 
+        
+        let v: Vec<&str> = s.split(",").collect();
+        if v.len() != 2 {
+            return Person::default();
+        }
+
+        if v[0].is_empty() {
+            return Person::default();
+        }
+        if v[1].parse::<usize>().is_err() {
+            return Person::default();
+        }
+
+        return Person {
+            name: v[0].to_string(),
+            age: v[1].parse::<usize>().unwrap(),
+        };
     }
 }
 
--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,8 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 /// # Safety
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
@@ -31,8 +29,11 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+
+    
     unsafe {
-        todo!("Your code goes here")
+        let addr = address as *mut usize;
+        //*addr = 0xAABBCCDD;
     }
 }
 
@@ -45,6 +46,8 @@ mod tests {
         let mut t: u32 = 0x12345678;
         // SAFETY: The address is guaranteed to be valid and contains
         // a unique reference to a `u32` local variable.
+        //let addr = &mut t as *mut u32 as usize;
+        //unsafe { modify_by_address(addr) };
         unsafe { modify_by_address(&mut t as *mut u32 as usize) };
         assert!(t == 0xAABBCCDD);
     }
